659690481c58c294606c147e1be72fa4a142fb93
sharad1126 <sharad.agarwal2014@vit.ac.in>
2023-09-16 12:50:26 +0100

    tabular.py updated for multiple tables in db

--- app/convertion/tabular.py
+++ app/convertion/tabular.py
@@ -25,6 +25,14 @@ def convert_from_db_to_csv(input_file: str, output_dir: str):
 			with open(output_path, 'a') as f:
 				writer = csv.writer(f, delimiter=';', lineterminator='\n')
 				writer.writerow(row)
+	if len(table_list) > 2:
+		for i in range(len(table_list)):
+			output_path = Path(output_dir) / (file_name) / (list(table_list[i])[0] + ".csv")
+			table_name = table_list[i]
+			for row in cur.execute("SELECT * FROM " + list(table_name)[0]):
+				with open(output_path, 'a') as f:
+					writer = csv.writer(f, delimiter=';', lineterminator='\n')
+					writer.writerow(row)
 
 def convert_other_to_csv(input_file: str, output_dir: str):
     file_name = Path(input_file).stem
